1a214636486fc29f2d0a26560df5b8f786843516
“Alex <alex@ruzzarin.net>
2015-10-24 22:50:07 -0300

    normalize

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,6 +9,7 @@ import eslint from 'gulp-eslint';
 import htmlmin from 'gulp-htmlmin';
 import onlyif from 'gulp-if';
 import imagemin from 'gulp-imagemin';
+import rename from 'gulp-rename';
 import sass from 'gulp-sass';
 import sourcemaps from 'gulp-sourcemaps';
 import uglify from 'gulp-uglify';
@@ -20,7 +21,7 @@ const source = {
     base: './src',
     js: ['src/scripts/**/*.js'],
     html: ['src/*.html'],
-    styles: ['src/styles/*.sass'],
+    styles: ['src/styles/*.scss'],
     images: ['src/images/**/*.*'],
 };
 
@@ -32,7 +33,7 @@ gulp.task('pre', () => {
         .pipe(david.reporter);
 });
 
-gulp.task('js', ['pre'], () => {
+gulp.task('js', () => {
     return gulp.src(source.js, {base: source.base})
         .pipe(eslint())
         .pipe(onlyif(!production, sourcemaps.init()))
@@ -40,17 +41,23 @@ gulp.task('js', ['pre'], () => {
         .pipe(onlyif(production, uglify()))
         .pipe(onlyif(!production, sourcemaps.write()))
         .pipe(gulp.dest("dist"))
-        .pipe(onlyif(!production, browserSync.reload({ stream: true })));
+        .pipe(onlyif(!production, browserSync.reload({stream: true})));
 });
 
-gulp.task('html', ['pre'], () => {
+gulp.task('html', () => {
     return gulp.src(source.html, {base: source.base})
         .pipe(onlyif(!production, htmlmin({collapseWhitespace: true})))
         .pipe(gulp.dest('dist'))
-        .pipe(onlyif(!production, browserSync.reload({ stream: true })));
+        .pipe(onlyif(!production, browserSync.reload({stream: true})));
 });
 
-gulp.task('css', ['pre'], () => {
+gulp.task('vendor-css', () => {
+    return gulp.src('node_modules/normalize.css/normalize.css')
+        .pipe(rename('_normalize.scss'))
+        .pipe(gulp.dest('./temp'));
+});
+
+gulp.task('css', ['vendor-css'], () => {
     return gulp.src(source.styles, {base: source.base})
         .pipe(onlyif(!production, sourcemaps.init()))
         .pipe(sass().on('error', sass.logError))
@@ -59,7 +66,7 @@ gulp.task('css', ['pre'], () => {
         .pipe(onlyif(!production, browserSync.stream()));
 });
 
-gulp.task('images', ['pre'], () => {
+gulp.task('images', () => {
     const options = {
         progressive: true,
         interlaced: true,
@@ -67,7 +74,7 @@ gulp.task('images', ['pre'], () => {
     return gulp.src(source.images, {base: source.base})
         .pipe(onlyif(!production, imagemin(options)))
         .pipe(gulp.dest('dist'))
-        .pipe(onlyif(!production, browserSync.reload({ stream: true })));
+        .pipe(onlyif(!production, browserSync.reload({stream: true})));
 });
 
 gulp.task('watch', ['default'], ()=> {
@@ -81,4 +88,4 @@ gulp.task('watch', ['default'], ()=> {
     gulp.watch(source.images, ['images']);
 });
 
-gulp.task('default', ['html', 'js', 'css', 'images']);
+gulp.task('default', ['pre', 'html', 'js', 'css', 'images']);
